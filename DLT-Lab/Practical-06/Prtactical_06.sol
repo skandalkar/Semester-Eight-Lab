565755d283456ae714d1bf9efd3bf9d9b14cd6bf
Santosh Kandalkar <104355707+skandalkar@users.noreply.github.com>
2026-02-05 08:51:39 +0000

    DLT Practical 06

---
+++ DLT-Lab/Practical-06/Prtactical_06.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.4;
+
+contract RestricedAccess {
+    address public owner = msg.sender;
+    uint public creationTime = block.timestamp;
+
+    modifier onlyBy(address _account) {
+        require(msg.sender == _account, "Sender not Authorized!");
+        _;
+    }
+
+    modifier onlyAfter(uint _time) {
+        require(block.timestamp >= _time, "Function was called too early!");
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "Not enough ether provided!");
+        _;
+    }
+
+    function forceChangeOwner(address _newOwner) payable public costs(200 ether) {
+        owner = _newOwner;
+    }
+
+    function changeOwner(address _owner) public onlyBy(owner) {
+        owner = _owner;
+    }
+
+    function disown() public onlyBy(owner) onlyAfter(creationTime + 5 seconds) {
+        delete owner;
+    }
+}
